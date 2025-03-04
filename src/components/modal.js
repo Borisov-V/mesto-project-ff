e3eeb08db8c8ccda09bb24253228c722899cf415
Borisov-V <spann1@yandex.ru>
2025-03-04 18:50:56 +0300

    add a constant for the button

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,3 +1,5 @@
+const keyCodeEscButton = 27;
+
 export function openModal(modal) {
   modal.classList.add('popup_is-opened');
 
@@ -11,7 +13,7 @@ export function closeModal(modal) {
 }
 
 function handleCloseByKey(evt) {
-  if (evt.keyCode === 27) {
+  if (evt.keyCode === keyCodeEscButton) {
     const popup = document.querySelector('.popup_is-opened');
 
     closeModal(popup);
